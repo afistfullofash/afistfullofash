887d06e9f4e99ec77beff596ac936d217d98c9d0
Natalie Atkinson <natalie.atkinson95@pm.me>
2026-02-20 06:38:55 +1100

    fix: dunst-catppuccin-theme path

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((jinx-dir-local-words . "catppuccin cattapuccin dunst"))))
+((nil . ((jinx-dir-local-words . "alacritty catppuccin cattapuccin color colors conf config dracula dunst dunstrc gtk kde lsd macchiato makefile py src starship toml yaml ð"))))
